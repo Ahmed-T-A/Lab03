1c487cd7a7bdf57dd2a8226fa1ecb42edf88d591
Ahmed Talal <hamad5232@yahoo.ca>
2023-02-06 17:09:52 +0300

    modifiying

--- Sorted.java
+++ Sorted.java
@@ -1,6 +1,6 @@
 public class Sorted{
   public static void main(String[] args) {
-    System.out.println("hello");
+    System.out.println("hello world");
   }
 
   public static void selectionSort(int[] arr){  
